ce793efc5ca194f3b72b917b17dc74a81d10f161
Bart Klaasse <bart@bartklaasse.nl>
2020-03-01 23:49:20 +0100

    commit to save no vid finished yet

--- Uplift/Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
+++ Uplift/Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Uplift.DataAccess.Data.Repository.IRepository;
 
 namespace Uplift.DataAccess.Data.IRepository
 {
-    interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
-
+        ICategoryRepository Category { get; }
+        void Save();
     }
 }
--- Uplift/Uplift.DataAccess/Data/Repository/UnitOfWork.cs
+++ Uplift/Uplift.DataAccess/Data/Repository/UnitOfWork.cs
@@ -2,11 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Uplift.DataAccess.Data.IRepository;
+using Uplift.DataAccess.Data.Repository.IRepository;
 
 namespace Uplift.DataAccess.Data.Repository
 {
-    class UnitOfWork
+    class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _db;
 
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
     }
 }
