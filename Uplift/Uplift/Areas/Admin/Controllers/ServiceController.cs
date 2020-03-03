6cc8e7aa5d4682a44fe40fcf60931586b77b0b28
Bart Klaasse <bart@bartklaasse.nl>
2020-03-03 13:33:58 +0100

    78 upsert post method new service

--- Uplift/Uplift/Areas/Admin/Controllers/ServiceController.cs
+++ Uplift/Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -17,6 +18,9 @@ namespace Uplift.Controllers
         private readonly IUnitOfWork _unitOfWOrk;
         private readonly IWebHostEnvironment _hostingEnvironment;
 
+        [BindProperty]
+        public ServiceViewModel ServiceVM { get; set; }
+
         public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnvironment)
         {
             _unitOfWOrk = unitOfWork;
@@ -30,7 +34,7 @@ namespace Uplift.Controllers
 
         public IActionResult Upsert(int? id)
         {
-            ServiceViewModel serviceVM = new ServiceViewModel()
+            ServiceVM = new ServiceViewModel()
             {
                 Service = new Models.Service(),
                 CategoryList = _unitOfWOrk.Category.GetCategoryListForDropDown(),
@@ -38,9 +42,37 @@ namespace Uplift.Controllers
             };
             if (id != null)
             {
-                serviceVM.Service = _unitOfWOrk.Service.Get(id.GetValueOrDefault());
+                ServiceVM.Service = _unitOfWOrk.Service.Get(id.GetValueOrDefault());
             }
-            return View(serviceVM);
+            return View(ServiceVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                if (ServiceVM.Service.Id == 0)
+                {
+                    // new service
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\services");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    using(var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStreams);
+                    }
+
+                    ServiceVM.Service.ImageUrl = @"images\services\" + fileName + extension;
+                    _unitOfWOrk.Service.Add(ServiceVM.Service);
+                }
+            }
+
+            return View();
         }
 
         #region API Calls
