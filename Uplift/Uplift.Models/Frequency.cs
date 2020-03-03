3367b0b473df047fe02b9d8479f0b930eb70b13b
Bart Klaasse <bart@bartklaasse.nl>
2020-03-03 10:34:23 +0100

    61 assigntment 1

---
+++ Uplift/Uplift.Models/Frequency.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uplift.Models
+{
+    public class Frequency
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Frequency Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Frequency Count")]
+        public int FrequencyCount { get; set; }
+
+    }
+}
