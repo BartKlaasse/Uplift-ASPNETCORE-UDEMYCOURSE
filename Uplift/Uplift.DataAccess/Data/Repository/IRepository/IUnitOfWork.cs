8dca3852accb52fe9334e3698999a8b19758f9b0
Bart Klaasse <bart@bartklaasse.nl>
2020-03-03 10:40:49 +0100

    65 assignment 3

--- Uplift/Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
+++ Uplift/Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Uplift.DataAccess.Data.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
+        IFrequencyRepository Frequency { get; }
         void Save();
     }
 }
