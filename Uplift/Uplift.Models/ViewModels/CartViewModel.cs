a49570b17376cd008244b043ae6b62b9960f1387
Bart Klaasse <bart@bartklaasse.nl>
2020-03-04 13:48:10 +0100

    96 cart viewmodel

---
+++ Uplift/Uplift.Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uplift.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IList<Service> ServiceList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
