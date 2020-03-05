f08b959efe8a2d5031e954511e74ccebae8a045f
Bart Klaasse <bart@bartklaasse.nl>
2020-03-05 11:47:23 +0100

    113 usercontroller index method

---
+++ Uplift/Uplift/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity) this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            return View(_unitOfWork.User.GetAll(u => u.Id != claims.Value));
+        }
+    }
+}
