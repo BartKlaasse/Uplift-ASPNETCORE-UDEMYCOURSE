51a72628ac7260a6000e093a55564fde79b52350
Bart Klaasse <bart@bartklaasse.nl>
2020-03-05 11:03:12 +0100

    109 add props to aspnetuser

--- Uplift/Uplift.DataAccess/Migrations/20200305100228_appusermodel2.Designer.cs
+++ Uplift/Uplift.DataAccess/Migrations/20200305100228_appusermodel2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Uplift.DataAccess.Data;
 
 namespace Uplift.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200305100228_appusermodel2")]
+    partial class appusermodel2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
