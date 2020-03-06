a8f6fb1f29061569b7516341d0871a9bf4679cc3
Bart Klaasse <bart@bartklaasse.nl>
2020-03-06 11:02:32 +0100

    133 stored proc Irepository

---
+++ Uplift/Uplift.DataAccess/Data/Repository/IRepository/SP_Call.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Uplift.DataAccess.Data.IRepository;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository.IRepository
+{
+    public interface IStoredProcRepository : IDisposable
+    {
+        IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null);
+
+        void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null);
+        void ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null);
+    }
+}
