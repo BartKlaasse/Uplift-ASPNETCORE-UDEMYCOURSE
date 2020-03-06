67c7dfd369ba2eb868a9686ab4256025c06b064b
Bart Klaasse <bart@bartklaasse.nl>
2020-03-06 11:35:56 +0100

    136 create webimages model

---
+++ Uplift/Uplift.Models/WebImages.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uplift.Models
+{
+    public class WebImages
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte[] Picture { get; set; }
+    }
+}
